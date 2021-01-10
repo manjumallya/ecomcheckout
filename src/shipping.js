0cb86348cdfc9592bdfc4de8f342e2d503ff022a
Manjunath Mallya Konaje <manjunath_40@yahoo.co.in>
2021-01-10 21:05:21 +0100

    Code changes for modifying the address and submit the address

--- src/shipping.js
+++ src/shipping.js
@@ -1,24 +1,35 @@
 import '@lion/input/lion-input.js';
 import {ajax} from "@lion/ajax";
 import {html, render} from "lit-html";
+import '@lion/fieldset/lion-fieldset.js';
 
-export let addressData
-export let correspondenceName, phoneNumber
+export let submittedAddress
+
 const formAddress = (addressData, name, phoneNumber) =>
                 html`
-                    <lion-input  .modelValue=${name} label="Correspondence Name"></lion-input>
-                    <lion-input  .modelValue=${addressData.street} label="Street Name"></lion-input>
-                    <lion-input  .modelValue=${addressData.houseNumber} label="House Number"></lion-input>
-                    <lion-input  .modelValue=${addressData.postalCode} label="Postal Code"></lion-input>
-                    <lion-input  .modelValue=${addressData.city} label="city"></lion-input>
-                    <lion-input  .modelValue=${phoneNumber} label="Phone Number"></lion-input>
+                <lion-fieldset name="nameGroup" id="fieldset" label="Name">
+                    <lion-input  name="name" .modelValue=${name} label="Correspondence Name"></lion-input>
+                    <lion-input  name="street" .modelValue=${addressData.street} label="Street Name"></lion-input>
+                    <lion-input  name="houseNumber" .modelValue=${addressData.houseNumber} label="House Number"></lion-input>
+                    <lion-input  name="postalCode" .modelValue=${addressData.postalCode} label="Postal Code"></lion-input>
+                    <lion-input  name="city" .modelValue=${addressData.city} label="city"></lion-input>
+                    <lion-input  name="phone" .modelValue=${phoneNumber} label="Phone Number"></lion-input>
+                     <button @click=${setAddressData}>
+                        Submit Address
+                    </button>
+                </lion-fieldset>
                 `;
+
+const setAddressData = (event) => {
+    event.currentTarget.disabled = true
+    submittedAddress = event.target.parentElement.modelValue
+}
 
 const formAddressData = async () => {
     await ajax.get('../data/address.json')
         .then(response => {
-            addressData = response.data.addresses[0];
-            ({correspondenceName, phoneNumber} = response.data)
+            const addressData = response.data.addresses[0];
+            const {correspondenceName, phoneNumber} = response.data
             render(formAddress(addressData, correspondenceName, phoneNumber), document.querySelector('#addressForm'))
         })
 }
--- src/thankyou.js
+++ src/thankyou.js
@@ -1,6 +1,6 @@
 import {html, render} from "lit-html";
 import{myList} from "./basket";
-import {addressData, correspondenceName, phoneNumber} from "./shipping";
+import {submittedAddress} from "./shipping";
 
 const thankYouMessage = () =>
     html `
@@ -9,12 +9,12 @@ const thankYouMessage = () =>
         ${myList}
         <h4>Delivery Address: </h4>
         <p>
-        ${correspondenceName} <br>
-        ${addressData.street} <br>
-        ${addressData.houseNumber} <br>
-        ${addressData.postalCode} <br>
-        ${addressData.city} <br>
-        ${phoneNumber}
+        ${submittedAddress.name} <br>
+        ${submittedAddress.street} <br>
+        ${submittedAddress.houseNumber} <br>
+        ${submittedAddress.postalCode} <br>
+        ${submittedAddress.city} <br>
+        ${submittedAddress.phone}
         </p>
     `
 export const getThankYouContent = () => {
