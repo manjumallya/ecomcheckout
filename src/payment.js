ef9c7c8eae962e7c6ff6ae1753d2174ba5e6e922
Manjunath Mallya Konaje <manjunath_40@yahoo.co.in>
2021-01-10 22:39:40 +0100

    Balance check in payment component

--- src/payment.js
+++ src/payment.js
@@ -8,6 +8,7 @@ const formAccount = (accountData) =>
                     <lion-input  readonly .modelValue=${accountData.number} label="Account Number"></lion-input>
                     <lion-input  readonly .modelValue=${accountData.name} label="Account Name"></lion-input>
                     <p>Payment Price: ${basketPrice}</p>
+                    <p id="errorBalance"></p>
                 `;
 
 const formAccountData = async () => {
@@ -15,9 +16,17 @@ const formAccountData = async () => {
         .then(response => {
             let accountData = response.data.accounts[0];
             render(formAccount(accountData), document.querySelector('#accountForm'))
+            if (isInsufficientBalance(accountData.amount, basketPrice)) {
+                $('#errorBalance').html('Insufficient balance')
+                document.getElementById('paymentNext').disabled = true
+            }
         })
 }
 
 export const getAccountDetails = async () => {
     let accountData = await formAccountData()
+}
+
+const isInsufficientBalance = (amount, basketPrice) => {
+    return amount - basketPrice < 0 ? true : false
 }
