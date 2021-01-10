58214bb9d224d553e9e815491eda91c13b01c85c
Manjunath Mallya Konaje <manjunath_40@yahoo.co.in>
2021-01-10 16:16:21 +0100

    Using mock json address provided

--- src/shipping.js
+++ src/shipping.js
@@ -2,20 +2,22 @@ import '@lion/input/lion-input.js';
 import {ajax} from "@lion/ajax";
 import {html, render} from "lit-html";
 
-const formAddress = (addressData) =>
+const formAddress = (addressData, name, phoneNumber) =>
                 html`
-                    <lion-input  .modelValue=${addressData.streetName} label="streetName"></lion-input>
-                    <lion-input  .modelValue=${addressData.doorNumber} label="doorNumber"></lion-input>
-                    <lion-input  .modelValue=${addressData.postalCode} label="postalCode"></lion-input>
+                    <lion-input  .modelValue=${name} label="Correspondence Name"></lion-input>
+                    <lion-input  .modelValue=${addressData.street} label="Street Name"></lion-input>
+                    <lion-input  .modelValue=${addressData.houseNumber} label="House Number"></lion-input>
+                    <lion-input  .modelValue=${addressData.postalCode} label="Postal Code"></lion-input>
                     <lion-input  .modelValue=${addressData.city} label="city"></lion-input>
-                    <lion-input  .modelValue=${addressData.country} label="country"></lion-input>
+                    <lion-input  .modelValue=${phoneNumber} label="Phone Number"></lion-input>
                 `;
 
 const formAddressData = async () => {
     await ajax.get('../data/address.json')
         .then(response => {
-            let addressData = response.data.addressList[0];
-            render(formAddress(addressData), document.querySelector('#addressForm'))
+            let addressData = response.data.addresses[0];
+            const {correspondenceName, phoneNumber} = response.data
+            render(formAddress(addressData, correspondenceName, phoneNumber), document.querySelector('#addressForm'))
         })
 }
 
