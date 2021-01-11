f94f67369632b96579157a77903c21d2511e3b43
C54794 <manjunath.mallya.konaje@nl.abnamro.com>
2021-01-12 00:07:27 +0100

    stub server configuration

---
+++ test/stubs/payment/index.js
@@ -0,0 +1,16 @@
+const accountData = require('./getAccountData.json')
+
+module.exports = ((server) => {
+    server.get('http://localhost:3000/getAccountData', ((req, res, next) => {
+        const base = 'usecase'
+        const option = req.headers.referer
+        const { pagename } = req.headers
+        const index = option.indexOf(base)
+        const request = req.headers.referer.substring(index + base.length + 1)
+
+        if (request === 'getAccountData') {
+            res.status(200).send(accountData)
+        }
+        return next()
+    }));
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const stubServerConfig = require('./test/stubs');
 module.exports = ({ mode }) => {
     return {
         mode: 'development',
+        devServer: {
+            before(app) {
+                stubServerConfig(app);
+            },
+            compress: true,
+            overlay: true,
+            open: true,
+            openPage: `index.html`,
+        },
         plugins: [
             new HtmlWebpackPlugin({
                 template: './src/index.html'
